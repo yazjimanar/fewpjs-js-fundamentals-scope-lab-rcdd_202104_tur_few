39c5e8f466caeef87021fd934f32662d15d3c1dc
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-20 13:01:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  return n + two;
 var two = 2;
   return n + two;
 
   // Feel free to move things around!
-  two = 2
+  two = 2;
 }
 
 }
