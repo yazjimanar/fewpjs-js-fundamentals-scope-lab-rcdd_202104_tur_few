31e0892bfe907ff1a8a36a70661dbf09d2520fbc
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-20 13:04:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-
 let animal = "cat";
-
 function myAnimal() {
   const animal = "dog";
-  return animal
+  return animal;
 }
 
-function yourAnimal() {
-
-}
 
 function add2(n) {
   return n + two;
 var two = 2;
   return n + two;
 
-  // Feel free to move things around!
-  two = 2;
-}
+
 
 }
